d7340677b171bc7d0d43dc53ef4a78241d2f741f
Dmitriy Kanaev <dimmkan@mail.ru>
2025-03-20 09:13:46 +0300

    Fix

--- internal/file/handler.go
+++ internal/file/handler.go
@@ -12,7 +12,9 @@ func NewFileHandler(router *http.ServeMux) {
 }
 
 func (handler *FileHandler) getFile() http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		http.ServeFile(w, req, "./static/cons-layer-api-v1_0-example.json");
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=windows-1251")
+		w.Header().Set("Content-Disposition", "attachment; filename=example.json")
+		http.ServeFile(w, r, "../internal/file/static/cons-layer-api-v1_0-example.json");
 	}
 }

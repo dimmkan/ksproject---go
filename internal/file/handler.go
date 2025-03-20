1737e5c2d378ce864d47293bd841493129853efd
Dmitriy Kanaev <dimmkan@mail.ru>
2025-03-20 10:14:41 +0300

    Fix

--- internal/file/handler.go
+++ internal/file/handler.go
@@ -26,8 +26,6 @@ func (handler *FileHandler) getFile() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=windows-1251")
 		w.Header().Set("Content-Disposition", "attachment; filename=example.json")
-		fmt.Println(handler.Config.FileStorage.STATIC_FILES_STORAGE)
-		fmt.Println(os.Executable())
 		http.ServeFile(w, r, fmt.Sprintf("%scons-layer-api-v1_0-example.json", handler.Config.FileStorage.STATIC_FILES_STORAGE));
 	}
 }

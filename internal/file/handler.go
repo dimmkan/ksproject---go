7706ae28b27d227333532ee9066be72c3ebfb24b
Dmitriy Kanaev <dimmkan@mail.ru>
2025-03-20 09:19:30 +0300

    debug

--- internal/file/handler.go
+++ internal/file/handler.go
@@ -1,7 +1,9 @@
 package file
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 type FileHandler struct{}
@@ -15,6 +17,7 @@ func (handler *FileHandler) getFile() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=windows-1251")
 		w.Header().Set("Content-Disposition", "attachment; filename=example.json")
-		http.ServeFile(w, r, "../internal/file/static/cons-layer-api-v1_0-example.json");
+		fmt.Println(os.Executable())
+		http.ServeFile(w, r, "../static/cons-layer-api-v1_0-example.json");
 	}
 }
